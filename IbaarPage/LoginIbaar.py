13ef9380ebd582e6673d09ed238e2cbe6f64bfe9
myubunto <saeedsamadyar@yahoo.com>
2022-08-23 21:48:46 -0700

    23/8/2022,Wed, Aug

--- IbaarPage/LoginIbaar.py
+++ IbaarPage/LoginIbaar.py
@@ -1,15 +1,16 @@
 class Login:
     def __init__(self, driver):
-        self.password_textbox_xpath = "ibaar_front_password"
-        self.username_textbox_xpath = "ibaar_front_username"
-        self.submit_button_xpath = "//button[@type='submit']//span"
+        self.submit_button_id = "(//span[@class='jss1340'])[2]"
+        self.password_textbox_id = "ibaar_front_password"
+        self.username_textbox_id = "ibaar_front_username"
+
         self.driver = driver
 
     def enter_username(self, username):
-        self.driver.find_element('id', self.username_textbox_xpath).send_keys(username)
+        self.driver.find_element('id', self.username_textbox_id).send_keys(username)
 
     def enter_password(self, password):
-        self.driver.find_element('id', self.password_textbox_xpath).send_keys(password)
+        self.driver.find_element('id', self.password_textbox_id).send_keys(password)
 
     def click_on_login_button(self):
-        self.driver.find_element('xpath', self.submit_button_xpath).click()
+        self.driver.find_element('xpath', self.submit_button_id).click()
--- IbaarTestCase/LoginIbaar.py
+++ IbaarTestCase/LoginIbaar.py
@@ -1,16 +1,23 @@
+from lib2to3.pgen2 import driver
+
 import login as login
 from selenium import webdriver
 # from webdriver.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import service
 from Pages.Login import Login
+from selenium.webdriver.common.keys import keys
+
 
 driver = webdriver.Chrome(executable_path="/home/myubunto/Downloads/chromedriver_linux64/chromedriver")
 # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 
-driver.get("https://front.ibaar.ir/login/")
-driver.implicitly_wait(1)
+driver.get("https://front.ibaar.ir/login")
+
+driver.implicitly_wait(3)
 
 Login = Login(driver=driver)
 # main_page = MainPage(driver=driver)
+
 
 login.enter_username()
 login.enter_password()
