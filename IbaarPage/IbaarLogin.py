f3b687fb881269ed3a30f23351b5ca22ba9376ee
myubunto <saeedsamadyar@yahoo.com>
2022-08-26 22:57:09 -0700

    27/8/2022

---
+++ IbaarPage/IbaarLogin.py
@@ -0,0 +1,16 @@
+class Login:
+    def __init__(self, driver):
+
+        self.submit_button_xpath = "//button[@type='submit']"
+        self.password_textbox_xpath = "//input[@type='password']"
+        self.username_textbox_xpath = "//input[@placeholder='Username']"
+        self.driver = driver
+
+    def enter_username(self, username):
+        self.driver.find_element('id', self.username_textbox_xpath).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element('id', self.password_textbox_xpath).send_keys(password)
+
+    def click_on_login_button(self):
+        self.driver.find_element('xpath', self.submit_button_xpath).click()
--- IbaarTestCase/LoginIbaar.py
+++ IbaarTestCase/LoginIbaar.py
@@ -5,13 +5,14 @@ from selenium import webdriver
 # from webdriver.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import service
 from Pages.Login import Login
-from selenium.webdriver.common.keys import keys
+
+# from selenium.webdriver.common.keys import keys
 
 
 driver = webdriver.Chrome(executable_path="/home/myubunto/Downloads/chromedriver_linux64/chromedriver")
 # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 
-driver.get("https://front.ibaar.ir/login")
+driver.get("https://front.ibaar.ir")
 
 driver.implicitly_wait(3)
 
@@ -22,4 +23,6 @@ Login = Login(driver=driver)
 login.enter_username()
 login.enter_password()
 login.click_on_login_button()
+
+
 # sleep(3)
